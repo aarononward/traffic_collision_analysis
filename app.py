f2509202beb7412a987c1cf4aca34e96ef0684fd
aarononward <129822330+aarononward@users.noreply.github.com>
2023-08-03 21:27:40 -0700

    adding beggining code for flask app setup

---
+++ app.py
@@ -0,0 +1,71 @@
+import sqlalchemy as sql
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import json
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = sql.create_engine("sqlite:///traffic_db.sqlite")
+
+# reflect an existing database into a new model
+#Base = automap_base()
+# reflect the tables
+#Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+#Data = Base.classes.traffic_db
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/data<br/>"
+        f"/accident_map<br/>"
+        f"/graphs"
+    )
+
+
+#All Data endpoint
+
+@app.route('/data')
+def return_data():
+    session = Session(engine)
+    
+    results= session.query(Data).all()
+
+    session.close()
+
+    sf_collisions=[]
+    for each_result in results:
+        collision = dict(each_result)
+        sf_collisions.append(collision)
+    return jsonify(sf_collisions)
+
+
+
+
+###################################################################
+#Nasr's Map end point#
+###################################################################
+@app.route('/accident_map')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
