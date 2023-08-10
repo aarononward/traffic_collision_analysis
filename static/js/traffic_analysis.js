9580e3f92befce2e762c49dcab3b39fd660a3fe5
aarononward <129822330+aarononward@users.noreply.github.com>
2023-08-09 20:35:11 -0700

    finalized cleaned and commented js code

--- static/js/traffic_analysis.js
+++ static/js/traffic_analysis.js
@@ -1,99 +1,110 @@
-//function createMap(trafficCollisions) {
-  // Create the tile layer that will be the background of our map.
-  let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// Create the tile layer that will be the background of our map.
+let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
 
 
-  // Initialize layergroups for collisions
+// Initialize layergroups for collisions
   let layers = {
     NO_HARM_COLLISIONS :  new L.layerGroup(),
     INJURY_COLLISIONS: new L.layerGroup(),
     FATAL_COLLISIONS: new L.layerGroup(),
     DUAL_HARM_COLLISIONS: new L.layerGroup()
-  };
+ };
 
-  //create map with layers
-  let map = L.map("map-id", {
-    center: [37.75709, -122.43979],
-    zoom: 12,
-    layers: [
-      layers.NO_HARM_COLLISIONS,
-      layers.FATAL_COLLISIONS,
-      layers.INJURY_COLLISIONS,
-      layers.DUAL_HARM_COLLISIONS
-    ]
-  });
+//create map with layers
+let map = L.map("map-id", {
+  center: [37.75709, -122.43979],
+  zoom: 12,
+  layers: [
+    layers.NO_HARM_COLLISIONS,
+    layers.FATAL_COLLISIONS,
+    layers.INJURY_COLLISIONS,
+    layers.DUAL_HARM_COLLISIONS
+  ]
+});
 
-  // Add "streetmap" tile layer to map
-  streetmap.addTo(map)
+// Add "streetmap" tile layer to map
+streetmap.addTo(map)
 
-  //create overlays 
-  let overlays = {
-     "Collisions without Injury or Death": layers.NO_HARM_COLLISIONS,
-     "Collisions with Injuries" : layers.INJURY_COLLISIONS,
-     "Collisions with Fatalities" : layers.FATAL_COLLISIONS,
-     "Collisions with Injuries & Fatalities"  : layers.DUAL_HARM_COLLISIONS
-  };
+//create overlays 
+let overlays = {
+  "Collisions without Injury or Death": layers.NO_HARM_COLLISIONS,
+  "Collisions with Injuries" : layers.INJURY_COLLISIONS,
+  "Collisions with Fatalities" : layers.FATAL_COLLISIONS,
+  "Collisions with Injuries & Fatalities"  : layers.DUAL_HARM_COLLISIONS
+};
  
  
-  //Create layer control and add the layer control to the map
-  L.control.layers(null,overlays, {
-     collapsed: false
-  }).addTo(map);
+//Create layer control and add the layer control to the map
+L.control.layers(null,overlays, {
+  collapsed: false
+}).addTo(map);
  
 
-  //Create legend
-  let collisionTotals = L.control({
+//Create legend
+let collisionTotals = L.control({
     position : "bottomright"
-    });
+});
 
-  //add html line connected to the legend
-  collisionTotals.onAdd = function() {
-    let div = L.DomUtil.create("div", "legend");
-    return div;
-  };
-  //Add the collision totals legend to the map
-  collisionTotals.addTo(map);
+//add html line connected to the legend
+collisionTotals.onAdd = function() {
+  let div = L.DomUtil.create("div", "legend");
+  return div;
+};
+
+//Add the collision totals legend to the map
+collisionTotals.addTo(map);
 
 //Initialize icon object for layer group
 let icons = {
-    NO_HARM_COLLISIONS: L.ExtraMarkers.icon({
-      icon: 'ion-alert-circle-outline',
-      iconColor: 'white',
-      markerColor: 'green',
-      shape: 'circle'
-    }),
-    INJURY_COLLISIONS: L.ExtraMarkers.icon({
-      icon: 'ion-medkit-outline',
-      iconColor: 'white',
-      markerColor: 'orange',
-      shape: 'square'
-    }),
-    FATAL_COLLISIONS: L.ExtraMarkers.icon({
-      icon: 'skull-sharp',
-      iconColor: 'white',
-      markerColor: 'purple',
-      shape: 'penta'
-    }),
-    DUAL_HARM_COLLISIONS: L.ExtraMarkers.icon({
-      icon: 'thumbs-up-outline',
-      iconColor: 'white',
-      markerColor: 'red',
-      shape: 'star'
-    })
+  NO_HARM_COLLISIONS: L.ExtraMarkers.icon({
+    icon: 'ion-alert-circle-outline',
+    iconColor: 'white',
+    markerColor: 'green',
+    shape: 'circle'
+  }),
+
+  INJURY_COLLISIONS: L.ExtraMarkers.icon({
+    icon: 'ion-medkit-outline',
+    iconColor: 'white',
+    markerColor: 'orange',
+    shape: 'square'
+  }),
+
+  FATAL_COLLISIONS: L.ExtraMarkers.icon({
+    icon: 'skull-sharp',
+    iconColor: 'white',
+    markerColor: 'purple',
+    shape: 'penta'
+  }),
+
+  DUAL_HARM_COLLISIONS: L.ExtraMarkers.icon({
+    icon: 'thumbs-up-outline',
+    iconColor: 'white',
+    markerColor: 'red',
+    shape: 'star'
+  })
 };
 
   // Perform an API call to the SFGOV API to get the crash information. Call createMarkers when it competes.
   d3.json("https://data.sfgov.org/resource/ubvf-ztfx.json").then(function(response){
+    
+    //create empty array for cleaned data
     let cleanCollisions= []
+    
+    //looping through api response
     for (let  i = 0; i< response.length; i++) {
-     let cleanCollision = response[i]
-     if (cleanCollision.tb_latitude !== null 
-      && cleanCollision.tb_latitude !== undefined 
-      && cleanCollision.tb_longitude !== null
-      && cleanCollision.tb_longitude !== undefined){
-       cleanCollisions.push(cleanCollision);
+      
+      //create a variable for each dictionary in the array
+      let cleanCollision = response[i]
+      
+      //exclude dictionaries with undefined and null lat and lon cells
+      if (cleanCollision.tb_latitude !== null 
+        && cleanCollision.tb_latitude !== undefined 
+        && cleanCollision.tb_longitude !== null
+        && cleanCollision.tb_longitude !== undefined){
+          cleanCollisions.push(cleanCollision);
       }
     };
    
@@ -109,8 +120,9 @@ let icons = {
     //create a collisionType variable
     let collisionType = "";
 
-    //loop through data and serperate into different types
+    //loop through data and serperate into different types based on injury and fatatlity counts
     for (let i=0; i< cleanCollisions.length; i++) {
+      
       let collision = cleanCollisions[i]      
       
       if (collision.number_injured <=0 && collision.number_killed <=0){
@@ -138,22 +150,25 @@ let icons = {
     });
 
     
+    //adding the markers to their layers
     collisionMarker.addTo(layers[collisionType]);
 
+    //adding pop up window for each marker with collsion type, injury count, and fatality count
     collisionMarker.bindPopup("Collision Type: " + collisionType + 
     "<br>" + "Injuries: " + collision.number_injured + 
     "<br>" + "Casualties: " + collision.number_killed);
   }
 
-  updateLegend(collisionCount);
-  });
+    //adding the collision count to the legend
+    updateLegend(collisionCount);
+    });
 
-  function updateLegend(collisionCount) {
-    document.querySelector(".legend").innerHTML = [
-      //"<p class = 'location'> Location: " + location + "</p>",
-      "<p class = 'no_harm_collision'> No-Harm Collisions: " + collisionCount.NO_HARM_COLLISIONS + "</p>",
-      "<p class = 'injury_collision'> Injury Collisions: " + collisionCount.INJURY_COLLISIONS + "</p>",
-      "<p class = 'fatal_collision'> Fatal Collisions: " + collisionCount.FATAL_COLLISIONS + "</p>",
-      "<p class = 'dual_harm_collision'> Dual-Harm Collisions: " + collisionCount.DUAL_HARM_COLLISIONS + "</p>" 
-    ].join("");
-  }
+    //updating the legend to reflect the counts of each collision type and tying each count to html
+    function updateLegend(collisionCount) {
+      document.querySelector(".legend").innerHTML = [
+        "<p class = 'no_harm_collision'> No-Harm Collisions: " + collisionCount.NO_HARM_COLLISIONS + "</p>",
+        "<p class = 'injury_collision'> Injury Collisions: " + collisionCount.INJURY_COLLISIONS + "</p>",
+        "<p class = 'fatal_collision'> Fatal Collisions: " + collisionCount.FATAL_COLLISIONS + "</p>",
+        "<p class = 'dual_harm_collision'> Dual-Harm Collisions: " + collisionCount.DUAL_HARM_COLLISIONS + "</p>" 
+      ].join("");
+    }
